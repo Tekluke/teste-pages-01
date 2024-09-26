7383eb858e76fdfb0325cc7eb73933253dd7ad3d
Micael Santos <trabalhodigitaltek@gmail.com>
2024-09-25 23:28:19 -0300

    ajustes 1.9

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,3 +1,23 @@
+const elements = document.querySelectorAll(".fadeL");
+
+elements.forEach((element) => {
+  ScrollReveal().reveal(element, {
+    origin: "left",
+    duration: 2000,
+    distance: "20%",
+  });
+});
+
+const elements2 = document.querySelectorAll(".fadeR");
+
+elements2.forEach((element) => {
+  ScrollReveal().reveal(element, {
+    origin: "right",
+    duration: 2000,
+    distance: "20%",
+  });
+});
+
 // Garante que a página role para o topo após todos os recursos serem carregados
 $(window).on("load", function () {
   setTimeout(function () {
