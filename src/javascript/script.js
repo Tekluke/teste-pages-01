907ce52a0e62e4c3cf105843e895b901585e54f7
Micael Santos <trabalhodigitaltek@gmail.com>
2024-09-26 14:54:07 -0300

    versão 1.0

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,30 +1,26 @@
 // Efeitos de Fade
-
 function handleAnimations() {
   const elements = document.querySelectorAll('.fadeL, .fadeR');
+
+  ScrollReveal().clean(); // Limpa animações anteriores
 
   elements.forEach(element => {
     const revealConfig = {
       duration: 2000,
+      reset: true,
+      origin: window.innerWidth <= 1200 ? 'bottom' : (element.classList.contains('fadeL') ? 'left' : 'right'),
+      distance: '20%'
     };
 
-    if (window.innerWidth <= 768) {
-      // Para telas menores ou iguais a 768px
-      revealConfig.origin = 'bottom';
-      revealConfig.distance = '20%';
-    } else {
-      // Para telas maiores que 768px
-      if (element.classList.contains('fadeL')) {
-        revealConfig.origin = 'left';
-      } else {
-        revealConfig.origin = 'right';
-      }
-      revealConfig.distance = '20%';
-    }
-
-    ScrollReveal().reveal(element, revealConfig);
+    // Utilizando requestAnimationFrame para garantir a atualização
+    requestAnimationFrame(() => {
+      ScrollReveal().reveal(element, revealConfig);
+    });
   });
 }
+
+// Chamar a função quando a página carregar
+window.onload = handleAnimations;
 
 // Garante que a página role para o topo após todos os recursos serem carregados
 $(window).on("load", function () {
