be806246b1d3af13647a83cbe7de9e4abe2519c6
Micael Santos <trabalhodigitaltek@gmail.com>
2024-09-26 13:48:22 -0300

    ajustes 2.6

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,22 +1,59 @@
-const elements = document.querySelectorAll(".fadeL");
+// Efeitos de Fade
 
-elements.forEach((element) => {
-  ScrollReveal().reveal(element, {
-    origin: "left",
-    duration: 2000,
-    distance: "10%",
-  });
-});
+function handleAnimations() {
+  // Verifica a largura da viewport
+  if (window.innerWidth <= 768) {
+    // Ocultar elementos fadeL e fadeR
+    const elementsL = document.querySelectorAll(".fadeL");
+    elementsL.forEach((element) => {
+      element.style.display = "none";
+    });
+    const elementsR = document.querySelectorAll(".fadeR");
+    elementsR.forEach((element) => {
+      element.style.display = "none";
+    });
 
-const elements2 = document.querySelectorAll(".fadeR");
+    // Mostrar e animar elementos fadeM
+    const elementsM = document.querySelectorAll(".fadeM");
+    elementsM.forEach((element) => {
+      element.style.display = "block";
+      ScrollReveal().reveal(element, {
+        origin: "bottom",
+        duration: 2000,
+        distance: "20%",
+      });
+    });
+  } else {
+    // Ocultar elementos fadeM
+    const elementsM = document.querySelectorAll(".fadeM");
+    elementsM.forEach((element) => {
+      element.style.display = "none";
+    });
 
-elements2.forEach((element) => {
-  ScrollReveal().reveal(element, {
-    origin: "right",
-    duration: 2000,
-    distance: "10%",
-  });
-});
+    //Efeitos de fade
+    const elementsL = document.querySelectorAll(".fadeL");
+    elementsL.forEach((element) => {
+      ScrollReveal().reveal(element, {
+        origin: "left",
+        duration: 2000,
+        distance: "20%",
+      });
+    });
+
+    const elementsR = document.querySelectorAll(".fadeR");
+    elementsR.forEach((element) => {
+      ScrollReveal().reveal(element, {
+        origin: "right",
+        duration: 2000,
+        distance: "20%",
+      });
+    });
+  }
+}
+
+// Chamar a função quando a página carregar e sempre que o tamanho da janela mudar
+window.onload = handleAnimations;
+window.addEventListener("resize", handleAnimations);
 
 // Garante que a página role para o topo após todos os recursos serem carregados
 $(window).on("load", function () {
