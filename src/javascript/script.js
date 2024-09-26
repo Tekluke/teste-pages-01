d4c5d013f83b85aa7c11ed4730a24f50a5adf27a
Micael Santos <trabalhodigitaltek@gmail.com>
2024-09-25 23:25:03 -0300

    ajustes 1.8

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,28 +1,3 @@
-$(document).ready(function () {
-  // Efeitos de Fade
-  const elements = document.querySelectorAll(".fadeL");
-
-  elements.forEach((element) => {
-    ScrollReveal().reveal(element, {
-      origin: "left",
-      duration: 2000,
-      distance: "20%",
-      reset: false,
-    });
-  });
-
-  const elements2 = document.querySelectorAll(".fadeR");
-
-  elements2.forEach((element) => {
-    ScrollReveal().reveal(element, {
-      origin: "right",
-      duration: 2000,
-      distance: "20%",
-      reset: false,
-    });
-  });
-});
-
 // Garante que a página role para o topo após todos os recursos serem carregados
 $(window).on("load", function () {
   setTimeout(function () {
